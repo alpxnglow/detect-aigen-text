db35cb3be46680263ce1b747e9f22fd30940cc26
alpxnglow <154473501+alpxnglow@users.noreply.github.com>
2023-12-22 15:24:00 -0800

    modified:   .DS_Store 	renamed:    docs/base_prompts -> data/base_prompts.txt 	new file:   data/edited_prompts.txt 	new file:   docs/.DS_Store 	modified:   docs/random_notes 	modified:   src/get_chatgpt_essays.py 	modified:   src/prompt_engineering.py

--- src/get_chatgpt_essays.py
+++ src/get_chatgpt_essays.py
@@ -1,20 +1,17 @@
 from openai import OpenAI
 client = OpenAI()
 
-# Prompt Engineering
-markerRemover = "Write a polished, final draft without paragraph markers"
-ages = ["10 year-old", "high-school student", "college student", "adult", "old person"]
-native = ["native English speaker", "non-native English speaker"]
-sourcesRemover = "Do not include a sources section"
+#edited prompts file
+path_file = '/Users/sanjayaharitsa/Downloads/projects/detect-aigen-text/detect-aigen-text/data/edited_prompts.txt'
+file = open(path_file, "r")
+line = file.readlines()
 
-response = client.chat.completions.create(
-  model="gpt-3.5-turbo",
-  messages=[
-    {"role": "system", "content": "You are a helpful assistant."},
-    {"role": "user", "content": "Who won the world series in 2020?"},
-    {"role": "assistant", "content": "The Los Angeles Dodgers won the World Series in 2020."},
-    {"role": "user", "content": "Where was it played?"}
-  ]
-)
-
-print(response)
+for i in range(100):
+  response = client.chat.completions.create(
+    model="gpt-3.5-turbo",
+    messages=[
+      {"role": "system", "content": "You are a helpful assistant."},
+      {"role": "user", "content": line[i]}
+    ]
+  )
+  print(response) #append this to a seperate text file later
---
+++ src/prompt_engineering.py
@@ -0,0 +1,30 @@
+#imports
+
+#tacks
+newPrompt =  ""
+markerRemover = "Write a polished, final draft without paragraph markers "
+ages = ["10 year-old", "high-school student", "college student", "adult", "old person"]
+native = ["native English speaker. ", "non-native English speaker. "]
+sourcesRemover = "Do not include a sources section."
+
+##extract first line of "base_prompts"
+path_file = '/Users/sanjayaharitsa/Downloads/projects/detect-aigen-text/detect-aigen-text/data/base_prompts.txt'
+new_path_file = '/Users/sanjayaharitsa/Downloads/projects/detect-aigen-text/detect-aigen-text/data/edited_prompts.txt'
+file = open(path_file, "r")
+new_file = open(new_path_file, "a")
+line = file.readlines()
+
+#print(line[2])
+
+
+#add tacks to prompt engineer
+for x in range(2):
+   for y in range(5):
+        for z in range(100):
+            newPrompt = line[z].strip() + " " + markerRemover + "in the voice of a " + ages[y] + " who is a " + native[x] + sourcesRemover
+            new_file.write(newPrompt)
+            new_file.write("\n")
+
+#close files
+file.close()
+new_file.close()
