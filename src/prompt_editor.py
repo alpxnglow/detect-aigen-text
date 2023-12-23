41fa5dd635dbc25de1b536738d50e6890dda13b9
alpxnglow <154473501+alpxnglow@users.noreply.github.com>
2023-12-23 14:14:01 -0800

    new file:   bert_classifier.pth 	modified:   src/prompt_editing.py

--- src/prompt_editor.py
+++ src/prompt_editor.py
@@ -1,6 +1,4 @@
-#imports
-
-#tacks
+#edits
 newPrompt =  ""
 markerRemover = 'Write a polished, final draft without paragraph markers (e.g., do not include "Title", "Introduction", "Paragraph 1", etc.) '
 ages = ["10 year-old", "high-school student", "college student", "adult", "old person"]
@@ -18,8 +16,13 @@ line = file.readlines()
 #print(line[2])
 
 
+<<<<<<< HEAD
 #add tacks to prompt engineer
 for x in range(100):
+=======
+#add edits to base prompt
+for x in range(len(native)):
+>>>>>>> e7d5add (	modified:   src/prompt_editing.py)
    for y in range(len(ages)):
         for z in range(len(native)):
             newPrompt = line[x].strip() + " " + markerRemover + "in the voice and style of a " + ages[y] + " who is a " + native[z] + sourcesRemover + errorIntro + ages[y] + "."
