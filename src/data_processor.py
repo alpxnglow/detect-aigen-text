c7e30f7805c002dbb76236bbd9336291ec5d197e
Prathap Harita <prathap_hr@hotmail.com>
2023-12-23 10:45:14 -0800

    modified:   data/edited_prompts.txt 	new file:   data/processed_prompts.txt 	new file:   data/training_data.csv 	modified:   src/data_processor.py 	renamed:    src/essay_retreival.py -> src/essay_retreiver.py

---
+++ src/data_processor.py
@@ -0,0 +1,21 @@
+import pandas as pd
+
+# Edited prompts file
+human_generated_essays_file = 'data/train_essays.csv'
+ai_generated_essays_file = 'data/ai_essays.csv'
+all_essays_file = 'data/training_data.csv'
+
+df_humans_data = pd.read_csv(human_generated_essays_file)
+df_ai_data = pd.read_csv(ai_generated_essays_file)
+
+df_humans_data.rename(columns = {'text':'essay'}, inplace = True)
+df_humans_data = df_humans_data.drop(['id', 'prompt_id'], axis=1)
+df_all_data = pd.concat([df_humans_data, df_ai_data], ignore_index=True, sort=False)
+df_all_data.to_csv(all_essays_file, encoding='utf-8', index=False)
+
+# print(df_humans_data)
+# print(df_humans_data.count())
+# print(df_ai_data)
+# print(df_ai_data.count())
+# print(df_all_data)
+# print(df_all_data.count())
