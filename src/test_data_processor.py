611a38105f6dfc09264bf7ce184c1a60d520a107
Prathap Harita <prathap_hr@hotmail.com>
2023-12-25 14:00:39 -0800

    new file:   data/0002b.xml 	modified:   src/test_data_processor.py

--- src/test_data_processor.py
+++ src/test_data_processor.py
@@ -1,23 +1,12 @@
-import pandas as pd
 import os
-import os
-import torch
-from torch import nn
-from torch.utils.data import DataLoader, Dataset
-from transformers import BertTokenizer, BertModel, get_linear_schedule_with_warmup
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score, classification_report
 import pandas as pd
 from pathlib import Path
 from datetime import datetime
 import xml.etree.ElementTree as ET
 
 HOME_DIR = str(Path.home())
-MODEL_OUTPUT_DIR = os.path.join(HOME_DIR, "Documents", "Model", str(datetime.now().strftime("%m%d%Y%H%M%S")))
-if not os.path.exists(MODEL_OUTPUT_DIR):
-    os.mkdir(MODEL_OUTPUT_DIR)
-MODEL_OUTPUT_LOCATION = os.path.join(MODEL_OUTPUT_DIR, "ai_human_essay_classifier.pth")
-
+HUMAN_ESSAYS_DIR = os.path.join(HOME_DIR, "Documents", "Misc", "data", "human_essays")
+FILE_PATH = os.path.join(HUMAN_ESSAYS_DIR, "0002b.xml")
 
 # Edited prompts file
 human_generated_essays_file = 'data/human_essays.csv'
